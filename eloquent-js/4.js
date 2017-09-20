3feab20300436287c19411989b5acedff183d3bc
footzi <footzi@mail.ru>
2017-09-20 14:04:29 +0300

    add tasks

--- eloquent-js/4.js
+++ eloquent-js/4.js
@@ -68,9 +68,58 @@ console.log(underFifty.map((person)=>{
 
 //функция reduce складывание элементов массива
 //свой вариант
-const reduce1 = (arr)=>{
+const reduce1 = (arr,start)=>{
+	let result=0;
 	for(let i=0;i<arr.length;i++) {
-		console.log(arr[i]);
+		if(arr[i]>=start){
+			result=result+arr[i];
+		}
 	}
+	return result
 };
-console.log(reduce1([1,2,3,4]))
+//console.log(reduce1([1,2,3,4],3));
+
+const reduce2 = (arr, combine, start)=>{
+	let current=start;
+	for( let i=0; i<arr.length; i++) {
+		current=combine(current,arr[i]);
+	}
+	return current;
+}
+//console.log(reduce2([1,2,3,4],(a,b)=>a+b,0))
+
+//функция определяющая средний возраст мужчин и женщин
+
+const averrage =(array)=>{
+	const plus=(a,b)=>a+b;
+	return array.reduce(plus)/array.length;
+}
+const male = (data) => data.sex=="m";
+const female = (data) => data.sex=="f";
+const age=(data)=>data.died-data.born;
+//console.log(averrage(data.filter(male).map(age)))
+//console.log(averrage(data.filter(female).map(age)))
+
+
+//Задание 1. Сверстка массивов.
+//Самостоятельно на 100%;
+let arrays=[[1,2],[4,5],[6,7,8,9,10],[11,12]]
+const test=(arr)=>{
+	return arr.reduce((a,b)=>a.concat(b));
+}
+//console.log(test(arrays));
+
+//Задание 2 Написать функцию every которая возвращает true, когла все условия true
+//самостоятельно на 80%
+const every=(arr,func)=>{
+	for (let i=0;i<arr.length;i++) {
+		if(!(func(arr[i]))) {
+			return true 
+		} else {
+			return false
+		}
+	}
+}
+
+console.log(every([NaN,NaN,4],isNaN));
+
---
+++ eloquent-js/5.js
@@ -0,0 +1,86 @@
+let rabbit={};
+rabbit.speak=(line)=>{
+	console.log("Кролик говорит: "+line);
+}
+rabbit.speak("Привет!");
+
+// const speak=(line)=>{
+// 	console.log("A "+this.type+" кролик говорит: "+line)
+// }
+function speak(line) {
+	console.log("A "+this.type+" кролик говорит: "+line)	
+}
+let whiteRabbit={type:"Белый",speak:speak};
+let fatRabbit={type:"Толстый",speak:speak};
+whiteRabbit.speak("Привет как дела?");
+
+let protoRabbis={
+	speak:function(line){
+		console.log(this.type+" кролик говорит: "+line);
+	}
+}
+let killerRabbits=Object.create(protoRabbis);
+killerRabbits.type="Убийственный"
+killerRabbits.speak("хрясь!!!")
+
+function Rabbits(type){
+	this.type=type;
+}
+let blackRabbit=new Rabbits("black");
+console.log(blackRabbit.type)
+
+console.log("////////////////////////////////////")
+//передача с помощью this ссылки на объект
+let user={
+	name:"Vladislav",
+	sayHi:function(){
+		showName(this)
+	}
+};
+const showName=(nameObj)=>console.log(nameObj.name);
+user.sayHi();
+
+//this(контекст вызова) функции известен только в момент вызова.
+let user1={name:"Vladislav"};
+let admin={name:"Alex"};
+function func(){
+	console.log(this.name)
+}
+user1.a=func
+admin.b=func
+user1.a();
+admin.b();
+
+const test=(str)=>{
+	let news="";
+	for(let i=0;i<str.length;i++) {
+		if(str[i]>"Z")
+			news=news+str[i].toUpperCase()
+		else {
+			news=news+str[i].toLowerCase()
+		}
+	}
+	return news
+
+}
+//console.log(test("hEllO cAt"));
+//test("HellO") 
+//console.log("A">"Z")
+
+const happy=(str)=>{
+
+	let left=String(str).substring(0,3);
+	let right=String(str).substring(3,6);
+	let leftS=0;
+	let rightS=0;
+	for (let i=0; i<left.length; i++) {
+		leftS=leftS+Number(left[i]);
+	}
+	for (let i=0; i<right.length; i++) {
+		rightS=rightS+Number(right[i]);
+	}
+	if(leftS==rightS) {
+		return true
+	} else return false
+}
+console.log(happy("054702"));
