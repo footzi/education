199258c93872d67f7e90670f73783f1ad7b52d57
footzi <footzi@mail.ru>
2017-10-10 07:08:34 +0300

    add feature

--- node/itvdn/express/primer/server.js
+++ node/itvdn/express/primer/server.js
@@ -1,45 +1,45 @@
-let express=require("express");
+let express = require("express");
 let app = express();
 let path = require("path");
 let fs = require("fs");
 
-let people=fs.readFileSync("data.json");
-people=JSON.parse(people);
+let people = fs.readFileSync("data.json");
+let log = "log.txt"
 
-let log="log.txt"
+people = JSON.parse(people);
 
 app.use("/", express.static(path.join(__dirname)))
 
-app.post("/test", function(req,res){
-	req.on("data", function(arr){
-		people=JSON.parse(arr);
-		console.log(people);
-		fs.writeFile("data.json",JSON.stringify(people),function(err) {
-			if (err) throw err;
-			console.log("write data file");
-		})
-	})
-	//res.send(JSON.stringify(people));; //конвертируем массив в jsonа а на клиенте конвертируем обратно
-	res.send(people);
+app.post("/test", function(req, res) {
+    req.on("data", function(arr) {
+        people = JSON.parse(arr);
+
+        fs.writeFile("data.json", JSON.stringify(people), function(err) {
+            if (err) throw err;
+            console.log("write data file");
+        })
+    })
+    res.send(people);
 })
 
-app.post("/send", function(req,res){
-	req.on("data", function(chunk){
-		people.push(JSON.parse(chunk));
-		fs.writeFile("data.json",JSON.stringify(people),function(err) {
-			if (err) throw err;
-			console.log("write data file");
-		})
+app.post("/send", function(req, res) {
+    req.on("data", function(chunk) {
+        people.push(JSON.parse(chunk));
 
-		let logger="address: " + req.ip + " data: " + chunk + "\n";
-		fs.appendFile(log,logger,function(err){
-			if (err) throw err;
-			console.log("write log");
-		})
-	})
+        fs.writeFile("data.json", JSON.stringify(people), function(err) {
+            if (err) throw err;
+            console.log("write data file");
+        })
+
+        let logger = "address: " + req.ip + " data: " + chunk + "\n";
+
+        fs.appendFile(log, logger, function(err) {
+            if (err) throw err;
+            console.log("write log");
+        })
+    })
 })
 
-
-app.listen("8080", function(){
-	console.log("server run")
+app.listen("8080", function() {
+    console.log("server run")
 });
